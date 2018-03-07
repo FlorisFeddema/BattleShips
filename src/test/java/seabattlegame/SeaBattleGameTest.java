f5ea8a1ad659ce94c101eb49642c3c766fc15086
Wolff,Roberto R.R.A. de <roberto.dewolff@student.fontys.nl>
2018-03-07 10:14:20 +0100

    Lege unit tests toegevoegd

---
+++ src/test/java/seabattlegame/SeaBattleGameTest.java
@@ -0,0 +1,49 @@
+package seabattlegame;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SeaBattleGameTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void registerPlayer() {
+    }
+
+    @Test
+    public void placeShipsAutomatically() {
+    }
+
+    @Test
+    public void placeShip() {
+    }
+
+    @Test
+    public void removeShip() {
+    }
+
+    @Test
+    public void removeAllShips() {
+    }
+
+    @Test
+    public void notifyWhenReady() {
+    }
+
+    @Test
+    public void fireShotPlayer() {
+    }
+
+    @Test
+    public void fireShotOpponent() {
+    }
+
+    @Test
+    public void startNewGame() {
+    }
+}
