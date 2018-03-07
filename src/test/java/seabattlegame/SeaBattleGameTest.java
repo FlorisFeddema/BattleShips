5f4495d34dd8cdced2ee10087f05cfaf8bfbe181
Wolff,Roberto R.R.A. de <roberto.dewolff@student.fontys.nl>
2018-03-07 11:24:55 +0100

    Place ships (automatically) tests aangemaakt

--- src/test/java/seabattlegame/SeaBattleGameTest.java
+++ src/test/java/seabattlegame/SeaBattleGameTest.java
@@ -4,35 +4,35 @@ import org.junit.Before;
 import org.junit.Test;
 import seabattlegui.ISeaBattleGUI;
 import seabattlegui.SeaBattleApplication;
+import seabattlegui.ShipType;
 import sun.plugin2.os.windows.SECURITY_ATTRIBUTES;
 
 import static org.junit.Assert.*;
 
 public class SeaBattleGameTest {
 
-    SeaBattleGame game;
+    ISeaBattleGame game;
+    ISeaBattleGUI ui;
 
     @Before
     public void setUp(){
         game = new SeaBattleGame();
+        ui = new SeaBattleApplication();
     }
 
     @Test
     public void singleplayerRegisterPlayerTest() {
-        ISeaBattleGUI ui = new SeaBattleApplication();
         assertEquals("Player not added", 0, game.registerPlayer("Dat boi", ui, true));
     }
 
     @Test
     public void singleplayerRegisterMultiplePlayersTest() {
-        ISeaBattleGUI ui = new SeaBattleApplication();
         assertEquals("Player not added", 0, game.registerPlayer("Dat boi", ui, true));
         assertEquals("Player invalidly added",-1, game.registerPlayer("Frans Bauer", ui, true));
     }
 
     @Test
     public void multiplayerRegisterPlayerTest() {
-        ISeaBattleGUI ui = new SeaBattleApplication();
         assertEquals("First player not only player", 0, game.registerPlayer("Henk", ui, false));
         assertEquals("Second player not added", 1, game.registerPlayer("Kaas", ui, false));
         assertEquals("Third player invalidly added", -1, game.registerPlayer("Hans", ui, false));
@@ -40,7 +40,6 @@ public class SeaBattleGameTest {
 
     @Test
     public void multiplayerRegisterDuplicatePlayerTest() {
-        ISeaBattleGUI ui = new SeaBattleApplication();
         assertEquals("First player not only player", 0, game.registerPlayer("Henk", ui, false));
         assertEquals("Second player invalidly added", -1, game.registerPlayer("Henk", ui, false));
         assertEquals("Second player not added", 1, game.registerPlayer("Troep", ui, false));
@@ -48,23 +47,32 @@ public class SeaBattleGameTest {
 
     @Test
     public void multiplayerRegisterSingleplayerPlayerTest() {
-        ISeaBattleGUI ui = new SeaBattleApplication();
         assertEquals("First player not added", 0, game.registerPlayer("Henk", ui, false));
         assertEquals("Second player not added", -1, game.registerPlayer("Henk", ui, false));
         assertEquals("Singleplayer player added to multiplayer game", -1, game.registerPlayer("Troep", ui, true));
     }
 
     @Test
-    public void placeShipsAutomatically() {
-
+    public void placeShipsAutomaticallyTest() {
+        game.registerPlayer("Henk", ui, true);
+        assertEquals("Ships not placed correctly", true, game.placeShipsAutomatically(0));
     }
 
     @Test
-    public void placeShip() {
+    public void placeShipTest() {
+        game.registerPlayer("Poep", ui, true);
+        assertEquals(true, game.placeShip(0, ShipType.MINESWEEPER, 0, 0, true));
+    }
+
+    @Test
+    public void placeShipOutOfGridTest() {
+        game.registerPlayer("Poep", ui, true);
+        assertEquals(false, game.placeShip(0, ShipType.MINESWEEPER, 10, 10, true));
     }
 
     @Test
     public void removeShip() {
+
     }
 
     @Test
