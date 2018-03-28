86e274e46ee46142d3e4c8061ca430fea0fe1e3e
Wolff,Roberto R.R.A. de <roberto.dewolff@student.fontys.nl>
2018-03-28 11:55:48 +0200

    Client en server klasses aangemaakt, moeten nog goed ingevuld worden

---
+++ src/main/java/client/EventClientSocket.java
@@ -0,0 +1,27 @@
+package client;
+
+import javax.websocket.*;
+
+@ClientEndpoint
+public class EventClientSocket {
+    @OnOpen
+    public void onWebSocketConnect() {
+        System.out.println("[Connected]");
+    }
+    @OnMessage
+    public void onWebSocketText(String message) {
+        System.out.println("[Received]: " + message);
+    }
+    @OnClose
+    public void onWebSocketClose(CloseReason reason) {
+        System.out.println("[Closed]: " + reason);
+    }
+    @OnError
+    public void onWebSocketError(Throwable cause) {
+        System.out.println("[ERROR]: " + cause.getMessage());
+    }
+
+    public void sendMessage(String message){
+
+    }
+}
---
+++ src/main/java/server/EventServer.java
@@ -0,0 +1,32 @@
+package server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+import javax.websocket.server.ServerContainer;
+import java.io.IOException;
+
+public class EventServer {
+    public static void main(String[] args){
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8096);
+        server.addConnector(connector);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        try{
+            ServerContainer container = WebSocketServerContainerInitializer.configureContext(context);
+            container.addEndpoint(EventServerSocket.class);
+            server.start();
+            server.join();
+        }
+        catch(Throwable t){
+            t.printStackTrace(System.err);
+        }
+    }
+}
